68b5926487cb93a35ae06082c545bfb7ae57bf45
FozzeY <tehfozzey@gmail.com>
2023-11-18 17:33:22 +0200

    Personal SSH key

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,3 +16,8 @@ resource "vultr_ssh_key" "ansible" {
   name    = "Ansible"
   ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJWRMiE+JFu2WAP2uOeaOoy11kdBz/7NWCdYow2mS5qA ansible@fozzey.ru"
 }
+
+resource "vultr_ssh_key" "personal" {
+  name    = "Personal"
+  ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK0c3l5L+HP1YKTfR7q0SG1lPLTon6PfFZHrjpplTbll admin@fozzey.ru"
+}
