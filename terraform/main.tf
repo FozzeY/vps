bb174caf197f2c033a43ca8e3c678322b53a9cfb
FozzeY <tehfozzey@gmail.com>
2023-11-18 17:09:49 +0200

    Pass Vultr secret

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,12 +12,6 @@ terraform {
   }
 }
 
-provider "vultr" {
-  api_key = ${{ secrets.VULTR_API_TOKEN }}
-  rate_limit = 100
-  retry_limit = 3
-}
-
 resource "vultr_ssh_key" "ansible" {
   name    = "Ansible"
   ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJWRMiE+JFu2WAP2uOeaOoy11kdBz/7NWCdYow2mS5qA ansible@fozzey.ru"
