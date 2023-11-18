defcc827696b1ffa965dacda7527049427ef4d44
FozzeY <tehfozzey@gmail.com>
2023-11-18 16:02:32 +0200

    Ansible SSH key

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,3 +7,8 @@ terraform {
     }
   }
 }
+
+resource "vultr_ssh_key" "ansible" {
+  name    = "Ansible"
+  ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJWRMiE+JFu2WAP2uOeaOoy11kdBz/7NWCdYow2mS5qA ansible@fozzey.ru"
+}
